398e4a2c90d68001f4b501cf2fdaed3a468bcf0d
educsng <eduardocsng@gmail.com>
2021-10-09 10:55:31 -0300

    Instanciado segundo filme e segundo diretor

--- aula02/exercicios-aula-2/src/main/java/Aplicacao.java
+++ aula02/exercicios-aula-2/src/main/java/Aplicacao.java
@@ -2,10 +2,13 @@ public class Aplicacao {
     public static void main(String[] args) {
 
         Diretor diretor = new Diretor("Christofer Nolan", 52, 20);
-        Filme filme = new Filme("Interestelar", "Ficção Científica no espaço", 280, 2018, 5,
-                diretor);
+        Diretor diretor2 = new Diretor("Martin Scorsese", 60, 40);
+        Filme filme1 = new Filme("Interestelar", "Ficção Científica no espaço", 280, 2018, 5, diretor);
+        Filme filme2 = new Filme("Ilha do Medo", "Suspense", 200, 2010, 5, diretor2);
 
-        filme.reproduzirFilme();
+        filme1.reproduzirFilme();
+        System.out.println("==================");
+        filme2.reproduzirFilme();
     }
 
 }
